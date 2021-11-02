73f28bbe448235c73a191dae679fa9e6238e90f1
ilyailya22 <82967979+ilyailya22@users.noreply.github.com>
2021-11-03 01:52:19 +0200

    delete user and password

--- ListTask/ListTask/ApplicationContext.cs
+++ ListTask/ListTask/ApplicationContext.cs
@@ -16,7 +16,7 @@ namespace ListTask
 
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
-            optionsBuilder.UseMySQL($"server=localhost;port=3307;database=tasks;user=ilya;password=Merihov-2002");
+            optionsBuilder.UseMySQL($"server=localhost;port=3307;database=tasks;user=user;password=password");
         }
     }
 }
